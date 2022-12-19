f41253e27ab6aa8fed455ebeda59c1fde2d24f5c
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2022-12-19 15:24:34 +0100

    Initial GCP installation script

--- installation/gcp-installation.sh
+++ installation/gcp-installation.sh
@@ -1,15 +1,67 @@
 #!/bin/sh
-# It insalls all required packages for running the experiments for the case when we are using GCP
+echo "Creating 8 VM instances on GCP"
+echo "It may take a while ..."
 
-python3.8 -m venv ../venv
-source ../venv/bin/activate
+gcloud deployment-manager deployments create hydragen-experiments --config ./gcp/deployment.yaml
+sleep 30
 
-echo 'Installing the requirements ... (it may take some minutes)'
+# Copying the installation scripts to the nodes
+echo "Copying all required scripts to the nodes ..."
+gcloud compute scp --recurse --zone "europe-north1-a" ./scripts  hydragen-k8s-master:~  > /dev/null 2>&1
+gcloud compute scp --recurse --zone "europe-north1-a" ./scripts  hydragen-k8s-worker-1:~  > /dev/null 2>&1
+gcloud compute scp --recurse --zone "europe-north1-a" ./scripts  hydragen-k8s-worker-2:~  > /dev/null 2>&1
+gcloud compute scp --recurse --zone "europe-north1-a" ./scripts  hydragen-k8s-worker-3:~  > /dev/null 2>&1
+gcloud compute scp --recurse --zone "europe-north1-a" ./scripts  hydragen-k8s-worker-4:~  > /dev/null 2>&1
+gcloud compute scp --recurse --zone "europe-north1-a" ./scripts  hydragen-k8s-worker-5:~  > /dev/null 2>&1
+gcloud compute scp --recurse --zone "europe-north1-a" ./scripts  hydragen-k8s-worker-6:~  > /dev/null 2>&1
+gcloud compute scp --recurse --zone "europe-north1-a" ./scripts  hydragen-k8s-worker-7:~  > /dev/null 2>&1
 
 
-if [[ $OSTYPE == 'darwin'* ]]; then
-  echo 'You are installing this script on MacOS, the installation may differ a bit ...'
-  pip install grpcio --use-pep517 &>/dev/null
-fi
+echo "\nInstalling required setup on Master node"
+echo "Installing Docker ..."
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-master"   --command "sudo bash ~/scripts/docker.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-master"   --command "sudo usermod -aG docker $USER"
+echo "Installing Kubernetes ..."
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-master"   --command "sudo bash ~/scripts/k8s.sh"
+echo "Initializing Kubernetes and installing Cilium CNI ..."
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-master"  --command "sudo kubeadm init --pod-network-cidr 10.244.0.0/16 > /dev/null 2>&1"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-master"  --command "bash ~/scripts/post-k8s.sh"
+var=$(gcloud compute ssh --zone 'europe-north1-a' hydragen-k8s-master  --command 'kubeadm token create --print-join-command')
 
-pip install -r ./gcp/requirements.txt &>/dev/null
+echo "\nInstalling required setup on Worker-1"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-1"   --command "sudo bash ~/scripts/docker.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-1"   --command "sudo bash ~/scripts/k8s.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-1"   --command "sudo $var  > /dev/null 2>&1"
+
+echo "\nInstalling required setup on Worker-2"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-2"   --command "sudo bash ~/scripts/docker.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-2"   --command "sudo bash ~/scripts/k8s.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-2"   --command "sudo $var  > /dev/null 2>&1"
+
+echo "\nInstalling required setup on Worker-3"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-3"   --command "sudo bash ~/scripts/docker.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-3"   --command "sudo bash ~/scripts/k8s.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-3"   --command "sudo $var  > /dev/null 2>&1"
+
+echo "\nInstalling required setup on Worker-4"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-4"   --command "sudo bash ~/scripts/docker.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-4"   --command "sudo bash ~/scripts/k8s.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-4"   --command "sudo $var  > /dev/null 2>&1"
+
+echo "\nInstalling required setup on Worker-5"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-5"   --command "sudo bash ~/scripts/docker.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-5"   --command "sudo bash ~/scripts/k8s.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-5"   --command "sudo $var  > /dev/null 2>&1"
+
+echo "\nInstalling required setup on Worker-6"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-6"   --command "sudo bash ~/scripts/docker.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-6"   --command "sudo bash ~/scripts/k8s.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-6"   --command "sudo $var  > /dev/null 2>&1"
+
+echo "\nInstalling required setup on Worker-7"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-7"   --command "sudo bash ~/scripts/docker.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-7"   --command "sudo bash ~/scripts/k8s.sh"
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-worker-7"   --command "sudo $var  > /dev/null 2>&1"
+
+echo "Installing Istio ..."
+gcloud compute ssh --zone "europe-north1-a" "hydragen-k8s-master"   --command "bash ~/scripts/mesh.sh"
