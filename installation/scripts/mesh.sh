76299969ddea8e9753c04f6dd4bb127d1d58e310
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2023-01-07 13:05:55 +0100

    add the script for installation with operator

--- installation/scripts/mesh.sh
+++ installation/scripts/mesh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Download Istio version 1.14.0
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.14.0 TARGET_ARCH=x86_64 sh - > /dev/null 2>&1
 
-~/istio-1.14.0/bin/istioctl install --set profile=default -y > /dev/null 2>&1
+# Install Istio with a custom operator
+ ~/istio-1.14.0/bin/istioctl install -y -f ~/scripts/configs/istio-operator.yaml > /dev/null 2>&1
