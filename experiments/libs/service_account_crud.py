6cd3a0126d062abdcddbdf78a1d772d74d3b6245
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2023-01-08 14:46:22 +0100

    add update_service_account func to SA CRUD

--- experiments/libs/service_account_crud.py
+++ experiments/libs/service_account_crud.py
@@ -10,7 +10,7 @@ def create_service_account(service_account):
     """
     try:
         api = client.CoreV1Api()
-        resp = api.create_namespaced_service(body=service_account, namespace=service_account['metadata']['namespace'])
+        resp = api.create_namespaced_service_account(body=service_account, namespace=service_account['metadata']['namespace'])
 
         logging.info("Service Account {} is successfully created. \n"
                      "\t Name \t\t Namespace\n"
@@ -22,4 +22,28 @@ def create_service_account(service_account):
                      )
     except client.ApiException as e:
         logging.warning("Service Account creation of {} did not completed, look at the following for more details".format(str(service_account['metadata']['name'])))
+        logging.warning(e)
+
+
+def update_service_account(service_account):
+    """
+    This function will update a kubernetes service_account
+    :param service_account:
+    :return:
+    """
+    try:
+        api = client.CoreV1Api()
+        resp = api.patch_namespaced_service_account(body=service_account, namespace=service_account['metadata']['namespace'],
+                                               name=service_account['metadata']['name'])
+
+        logging.info("Service Account {} is successfully updated. \n"
+                     "\t Name \t\t Namespace \n"
+                     "\t {} \t {}"
+                     "".format(service_account['metadata']['name'],
+                               resp.metadata.name,
+                               resp.metadata.namespace,
+                               )
+                     )
+    except client.ApiException as e:
+        logging.warning("Service Account update of {} did not completed, look at the following for more details".format(str(service_account['metadata']['name'])))
         logging.warning(e)
