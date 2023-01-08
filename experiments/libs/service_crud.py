c867ef694ad2db8c41b906ff328a00d636a6938c
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2023-01-08 14:32:44 +0100

    add update_service function to service CRUD

--- experiments/libs/service_crud.py
+++ experiments/libs/service_crud.py
@@ -23,3 +23,27 @@ def create_service(service):
     except client.ApiException as e:
         logging.warning("Service creation of {} did not completed, look at the following for more details".format(str(service['metadata']['name'])))
         logging.warning(e)
+
+
+def update_service(service):
+    """
+    This function will update a kubernetes service
+    :param service:
+    :return:
+    """
+    try:
+        api = client.CoreV1Api()
+        resp = api.patch_namespaced_service(body=service, namespace=service['metadata']['namespace'],
+                                               name=service['metadata']['name'])
+
+        logging.info("Service {} is successfully updated. \n"
+                     "\t Name \t\t Namespace \n"
+                     "\t {} \t {}"
+                     "".format(service['metadata']['name'],
+                               resp.metadata.name,
+                               resp.metadata.namespace,
+                               )
+                     )
+    except client.ApiException as e:
+        logging.warning("Service update of {} did not completed, look at the following for more details".format(str(configmap['metadata']['name'])))
+        logging.warning(e)
