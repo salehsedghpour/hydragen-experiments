f9f5ebabfd682144d4bf0cc3f03130a188ef6583
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2023-01-08 12:17:32 +0100

    add create_configmap function

---
+++ experiments/libs/configmap_crud.py
@@ -0,0 +1,25 @@
+import logging.config
+from kubernetes import client
+
+
+def create_configmap(configmap):
+    """
+    This function will create a kubernetes configmap
+    :param configmap:
+    :return:
+    """
+    try:
+        api = client.CoreV1Api()
+        resp = api.create_namespaced_config_map(body=configmap, namespace=configmap['metadata']['namespace'])
+
+        logging.info("Configmap {} is successfully created. \n"
+                     "\t Name \t\t Namespace\n"
+                     "\t {} \t {}"
+                     "".format(configmap['metadata']['name'],
+                               resp.metadata.name,
+                               resp.metadata.namespace,
+                               )
+                     )
+    except client.ApiException as e:
+        logging.warning("Configmap creation of {} did not completed, look at the following for more details".format(str(configmap['metadata']['name'])))
+        logging.warning(e)
