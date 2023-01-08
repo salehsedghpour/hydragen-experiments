faf01c5a7fbad99b9be1ac83e9176cf2c1dff10c
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2023-01-08 14:18:44 +0100

    add update_deployment function to deployment CRUD

--- experiments/libs/deployment_crud.py
+++ experiments/libs/deployment_crud.py
@@ -24,3 +24,28 @@ def create_deployment(deployment):
     except client.ApiException as e:
         logging.warning("Deployment creation of {} did not completed, look at the following for more details".format(str(deployment['metadata']['name'])))
         logging.warning(e)
+
+
+def update_deployment(deployment):
+    """
+    This function will update a kubernetes deployment
+    :param deployment:
+    :return:
+    """
+    try:
+        api = client.AppsV1Api()
+        resp = api.patch_namespaced_deployment(body=deployment, namespace=deployment['metadata']['namespace'],
+                                               name=deployment['metadata']['name'])
+
+        logging.info("Deployment {} is successfully updated. \n"
+                     "\t Name \t\t Namespace \t Image \n"
+                     "\t {} \t {} \t {}"
+                     "".format(deployment['metadata']['name'],
+                               resp.metadata.name,
+                               resp.metadata.namespace,
+                               resp.spec.template.spec.containers[0].image
+                               )
+                     )
+    except client.ApiException as e:
+        logging.warning("Deployment update of {} did not completed, look at the following for more details".format(str(deployment['metadata']['name'])))
+        logging.warning(e)
