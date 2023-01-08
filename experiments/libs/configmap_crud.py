33f9b0697f90f0463e10c0c6fbe23ba4015e3509
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2023-01-08 12:18:26 +0100

    add update_configmap function

--- experiments/libs/configmap_crud.py
+++ experiments/libs/configmap_crud.py
@@ -23,3 +23,27 @@ def create_configmap(configmap):
     except client.ApiException as e:
         logging.warning("Configmap creation of {} did not completed, look at the following for more details".format(str(configmap['metadata']['name'])))
         logging.warning(e)
+
+
+def update_configmap(configmap):
+    """
+    This function will update a kubernetes configmap
+    :param configmap:
+    :return:
+    """
+    try:
+        api = client.CoreV1Api()
+        resp = api.patch_namespaced_config_map(body=configmap, namespace=configmap['metadata']['namespace'],
+                                               name=configmap['metadata']['name'])
+
+        logging.info("Configmap {} is successfully updated. \n"
+                     "\t Name \t\t Namespace \n"
+                     "\t {} \t {}"
+                     "".format(configmap['metadata']['name'],
+                               resp.metadata.name,
+                               resp.metadata.namespace,
+                               )
+                     )
+    except client.ApiException as e:
+        logging.warning("Configmap update of {} did not completed, look at the following for more details".format(str(configmap['metadata']['name'])))
+        logging.warning(e)
